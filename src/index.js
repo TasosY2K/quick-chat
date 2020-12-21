38e04693967f21536aad2b3b9a521228b7f507da
TasosY2K <gustavolama20@gmail.com>
2020-12-21 06:35:27 +0200

    progress

--- src/index.js
+++ src/index.js
@@ -1,70 +1,120 @@
-//add sqlite and keep last 50 messages
-//add settings
 //add room system
+//keep last 50 messages
+//add settings
 //add deadman switch
 //modernize ui
 //add avatar
 //add meta data
 
 require("dotenv").config();
-let express = require('express');
-let app = express();
-let server = require('http').createServer(app);
-let io = require('socket.io')(server);
-let port = process.env.PORT || 3000;
+
+const sqlite3 = require('sqlite3').verbose();
+const { v4: uuidv4 } = require('uuid');
+
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log('Server listening at port %d', port);
+  console.log("Server listening at port %d", port);
 });
 
-app.use('/', express.static('dist'));
+const io = require("socket.io")(server);
+
+const db = new sqlite3.Database('./storage.db');
+
+db.each(`CREATE TABLE IF NOT EXISTS rooms (
+  room_id TEXT NOT NULL
+);`, err => {
+  if (err) console.error(err.message);
+});
+
+db.each(`CREATE TABLE IF NOT EXISTS messages (
+  room_id TEXT NOT NULL,
+  message_id TEXT NOT NULL,
+  user TEXT NOT NULL,
+  content TEXT NOT NULL
+);`, err => {
+  if (err) console.error(err.message);
+});
+
+app.use("/", express.static("dist"));
+
+app.get("/createroom", (req, res) => {
+  const id = uuidv4();
+  db.get(`INSERT INTO rooms (room_id) VALUES (?)`, [id], err => {
+    if (err) {
+      console.error(err.message);
+      res.status(503).send("Server error");
+    } else {
+      res.status(200).send(id);
+    }
+  });
+});
+
+app.get("/checkroom/:id", (req, res) => {
+  const id = req.params.id;
+  db.get(`SELECT room_id FROM rooms WHERE room_id = ?`, [id], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(503).send("Server error");
+    } else {
+      if (!row) {
+        res.status(404).send("Room not found");
+      } else {
+        res.status(200).send("Room found");
+      }
+    }
+  });
+});
 
 let numUsers = 0;
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   let addedUser = false;
 
-  socket.on('new message', data => {
-    socket.broadcast.emit('new message', {
+  socket.on("new message", (data) => {
+    socket.broadcast.emit("new message", {
       username: socket.username,
-      message: data
+      message: data,
     });
   });
 
-  socket.on('add user', username => {
+  socket.on("add user", (username) => {
     if (addedUser) return;
 
     socket.username = username;
     ++numUsers;
     addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
+    socket.emit("login", {
+      numUsers: numUsers,
     });
-    socket.broadcast.emit('user joined', {
+    socket.broadcast.emit("user joined", {
       username: socket.username,
-      numUsers: numUsers
+      numUsers: numUsers,
     });
   });
 
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
+  socket.on("typing", () => {
+    socket.broadcast.emit("typing", {
+      username: socket.username,
     });
   });
 
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
+  socket.on("stop typing", () => {
+    socket.broadcast.emit("stop typing", {
+      username: socket.username,
     });
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     if (addedUser) {
       --numUsers;
-      socket.broadcast.emit('user left', {
+      socket.broadcast.emit("user left", {
         username: socket.username,
-        numUsers: numUsers
+        numUsers: numUsers,
       });
-   }
+    }
   });
 });
--- src/static/main.js
+++ src/static/main.js
@@ -1,27 +1,37 @@
-import $ from 'jquery';
-import { io } from 'socket.io-client';
-import Push from 'push.js';
+import $ from "jquery";
+import { io } from "socket.io-client";
+import Push from "push.js";
 
-import './style.css';
+import "./style.css";
 
 let FADE_TIME = 150;
 let TYPING_TIMER_LENGTH = 400;
 let COLORS = [
-  '#e21400', '#91580f', '#f8a700', '#f78b00',
-  '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-  '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  "#e21400",
+  "#91580f",
+  "#f8a700",
+  "#f78b00",
+  "#58dc00",
+  "#287b00",
+  "#a8f07a",
+  "#4ae8c4",
+  "#3b88eb",
+  "#3824aa",
+  "#a700ff",
+  "#d300e7",
 ];
 
+let query = window.location.search;
 let $window = $(window);
-let $usernameInput = $('.usernameInput');
-let $messages = $('.messages');
-let $inputMessage = $('.inputMessage');
+let $usernameInput = $(".usernameInput");
+let $messages = $(".messages");
+let $inputMessage = $(".inputMessage");
 
-let $loginPage = $('.login.page');
-let $chatPage = $('.chat.page');
+let $loginPage = $(".login.page");
+let $chatPage = $(".chat.page");
 
 let username;
-let storedUsername = localStorage.getItem('username');
+let storedUsername = localStorage.getItem("username");
 let connected = false;
 let typing = false;
 let lastTypingTime;
@@ -33,54 +43,54 @@ if (storedUsername) {
   username = storedUsername;
   $loginPage.fadeOut();
   $chatPage.show();
-  $loginPage.off('click');
+  $loginPage.off("click");
   $currentInput = $inputMessage.focus();
 
-  socket.emit('add user', username);
+  socket.emit("add user", username);
 }
 
-const addParticipantsMessage = data => {
-  let message = '';
+const addParticipantsMessage = (data) => {
+  let message = "";
   if (data.numUsers === 1) {
     message += "there's 1 members online";
   } else {
     message += "there are " + data.numUsers + " members online";
   }
   log(message);
-}
+};
 
 const setUsername = () => {
   username = cleanInput($usernameInput.val().trim());
 
   if (username) {
-    localStorage.setItem('username', username);
+    localStorage.setItem("username", username);
     $loginPage.fadeOut();
     $chatPage.show();
-    $loginPage.off('click');
+    $loginPage.off("click");
     $currentInput = $inputMessage.focus();
 
-    socket.emit('add user', username);
+    socket.emit("add user", username);
   }
-}
+};
 
 const sendMessage = () => {
   let message = $inputMessage.val();
   message = cleanInput(message);
   if (message && connected) {
-    $inputMessage.val('');
+    $inputMessage.val("");
     addChatMessage({
       username: username,
-      message: message
+      message: message,
     });
 
-    socket.emit('new message', message);
+    socket.emit("new message", message);
   }
-}
+};
 
 const log = (message, options) => {
-  let $el = $('<li>').addClass('log').text(message);
+  let $el = $("<li>").addClass("log").text(message);
   addMessageElement($el, options);
-}
+};
 
 const addChatMessage = (data, options) => {
   let $typingMessages = getTypingMessages(data);
@@ -92,30 +102,29 @@ const addChatMessage = (data, options) => {
 
   let $usernameDiv = $('<span class="username"/>')
     .text(data.username)
-    .css('color', getUsernameColor(data.username));
-  let $messageBodyDiv = $('<span class="messageBody">')
-    .text(data.message);
+    .css("color", getUsernameColor(data.username));
+  let $messageBodyDiv = $('<span class="messageBody">').text(data.message);
 
-  let typingClass = data.typing ? 'typing' : '';
+  let typingClass = data.typing ? "typing" : "";
   let $messageDiv = $('<li class="message"/>')
-    .data('username', data.username)
+    .data("username", data.username)
     .addClass(typingClass)
     .append($usernameDiv, $messageBodyDiv);
 
   addMessageElement($messageDiv, options);
-}
+};
 
-const addChatTyping = data => {
+const addChatTyping = (data) => {
   data.typing = true;
-  data.message = 'is typing';
+  data.message = "is typing";
   addChatMessage(data);
-}
+};
 
-const removeChatTyping = data => {
+const removeChatTyping = (data) => {
   getTypingMessages(data).fadeOut(() => {
     $(this).remove();
   });
-}
+};
 
 const addMessageElement = (el, options) => {
   let $el = $(el);
@@ -123,10 +132,10 @@ const addMessageElement = (el, options) => {
   if (!options) {
     options = {};
   }
-  if (typeof options.fade === 'undefined') {
+  if (typeof options.fade === "undefined") {
     options.fade = true;
   }
-  if (typeof options.prepend === 'undefined') {
+  if (typeof options.prepend === "undefined") {
     options.prepend = false;
   }
   if (options.fade) {
@@ -139,54 +148,54 @@ const addMessageElement = (el, options) => {
   }
 
   $messages[0].scrollTop = $messages[0].scrollHeight;
-}
+};
 
-const cleanInput = input => {
-  return $('<div/>').text(input).text();
-}
+const cleanInput = (input) => {
+  return $("<div/>").text(input).text();
+};
 
 const updateTyping = () => {
   if (connected) {
     if (!typing) {
       typing = true;
-      socket.emit('typing');
+      socket.emit("typing");
     }
-    lastTypingTime = (new Date()).getTime();
+    lastTypingTime = new Date().getTime();
 
     setTimeout(() => {
-      let typingTimer = (new Date()).getTime();
+      let typingTimer = new Date().getTime();
       let timeDiff = typingTimer - lastTypingTime;
       if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-        socket.emit('stop typing');
+        socket.emit("stop typing");
         typing = false;
       }
     }, TYPING_TIMER_LENGTH);
   }
-}
+};
 
-const getTypingMessages = data => {
-  return $('.typing.message').filter(function (i) {
-    return $(this).data('username') === data.username;
+const getTypingMessages = (data) => {
+  return $(".typing.message").filter(function (i) {
+    return $(this).data("username") === data.username;
   });
-}
+};
 
-const getUsernameColor = username => {
+const getUsernameColor = (username) => {
   let hash = 7;
   for (let i = 0; i < username.length; i++) {
     hash = username.charCodeAt(i) + (hash << 5) - hash;
   }
   let index = Math.abs(hash % COLORS.length);
   return COLORS[index];
-}
+};
 
-$window.keydown(event => {
+$window.keydown((event) => {
   if (!(event.ctrlKey || event.metaKey || event.altKey)) {
     $currentInput.focus();
   }
   if (event.which === 13) {
     if (username) {
       sendMessage();
-      socket.emit('stop typing');
+      socket.emit("stop typing");
       typing = false;
     } else {
       setUsername();
@@ -194,7 +203,7 @@ $window.keydown(event => {
   }
 });
 
-$inputMessage.on('input', () => {
+$inputMessage.on("input", () => {
   updateTyping();
 });
 
@@ -206,41 +215,41 @@ $inputMessage.click(() => {
   $inputMessage.focus();
 });
 
-socket.on('login', data => {
+socket.on("login", (data) => {
   connected = true;
   let message = "Welcome to Quick Chat! – ";
   log(message, {
-    prepend: true
+    prepend: true,
   });
   addParticipantsMessage(data);
   Push.Permission.request();
 });
 
-socket.on('new message', data => {
+socket.on("new message", (data) => {
   addChatMessage(data);
   Push.create(data.username + " sent a message", {
-      body: data.message,
-      timeout: 1500
+    body: data.message,
+    timeout: 2300,
   });
 });
 
-socket.on('user joined', data => {
-  log(data.username + ' joined');
+socket.on("user joined", (data) => {
+  log(data.username + " joined");
   addParticipantsMessage(data);
-  Push.create(data.username + " joined the chat", {timeout: 1500});
+  Push.create(data.username + " joined the chat", { timeout: 2300 });
 });
 
-socket.on('user left', data => {
-  log(data.username + ' left');
+socket.on("user left", (data) => {
+  log(data.username + " left");
   addParticipantsMessage(data);
   removeChatTyping(data);
-  Push.create(data.username + " left the chat", {timeout: 1500});
+  Push.create(data.username + " left the chat", { timeout: 2300 });
 });
 
-socket.on('typing', data => {
+socket.on("typing", (data) => {
   addChatTyping(data);
 });
 
-socket.on('stop typing', data => {
+socket.on("stop typing", (data) => {
   removeChatTyping(data);
 });
